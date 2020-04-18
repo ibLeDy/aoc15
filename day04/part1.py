1fe9a7dcf88dc677a2205b34cc2a00b8b1d6c9e7
bLeDy <deejaynof@gmail.com>
2020-04-18 18:32:51 +0200

    Add day 4

---
+++ day04/part1.py
@@ -0,0 +1,12 @@
+import hashlib
+
+with open('input.txt', 'r') as f:
+    secret_key = f.read()
+
+hashed = ''
+number = -1
+while not hashed.startswith('00000'):
+    number += 1
+    hashed = hashlib.md5(f'{secret_key}{str(number)}'.encode()).hexdigest()
+
+print(number)
